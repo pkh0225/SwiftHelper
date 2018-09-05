79ec063a9b7c98e464abcd6d171de8ed234f7219
박길호 <152319@shinsegae.com>
2018-09-05 12:55:44 +0900

    no message

--- Class/StringExtensions.swift
+++ Class/StringExtensions.swift
@@ -928,3 +928,39 @@ extension NSAttributedString {
     }
 }
 
+extension String {
+    var lastPathComponent: String {
+        get {
+            return (self as NSString).lastPathComponent
+        }
+    }
+    var pathExtension: String {
+        get {
+            return (self as NSString).pathExtension
+        }
+    }
+    var stringByDeletingLastPathComponent: String {
+        get {
+            return (self as NSString).deletingLastPathComponent
+        }
+    }
+    var stringByDeletingPathExtension: String {
+        get {
+            return (self as NSString).deletingPathExtension
+        }
+    }
+    var pathComponents: [String] {
+        get {
+            return (self as NSString).pathComponents
+        }
+    }
+    func stringByAppendingPathComponent(path: String) -> String {
+        let nsSt = self as NSString
+        return nsSt.appendingPathComponent(path)
+    }
+    
+    func stringByAppendingPathExtension(ext: String) -> String? {
+        let nsSt = self as NSString
+        return nsSt.appendingPathExtension(ext)
+    }
+}
