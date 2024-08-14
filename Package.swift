257f0e0eed7d6202926629a538cd820ee5321621
박길호 <pkh0225@ssg.com>
2024-08-14 12:43:13 +0900

    샘플 프로젝트 이름 변경

--- Package.swift
+++ Package.swift
@@ -11,16 +11,12 @@ let package = Package(
             name: "SwiftHelper",
             targets: ["SwiftHelper"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/jaejinmanse/Nuke", from: "9.1.3"),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.3.0"),
-        .package(url: "https://github.com/pkh0225/EasyConstraints", from: "0.1.5")
-    ],
+    dependencies: [ ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftHelper",
-            dependencies: ["Nuke", "Alamofire", "EasyConstraints"]),
+            dependencies: [),
     ]
 )
