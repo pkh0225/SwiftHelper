af1a825d8819720c247d88e9217c454db8e1b1cd
박길호 <pkh0225@ssg.com>
2025-03-13 16:55:26 +0900

    Update TimerExtensions.swift
     timer 수정

--- Sources/SwiftHelper/Data+Extention/TimerExtensions.swift
+++ Sources/SwiftHelper/Data+Extention/TimerExtensions.swift
@@ -9,25 +9,38 @@
 import Foundation
 
 extension Timer {
-    ///   Runs every x seconds, to cancel use: timer.invalidate()
+    // 지정된 횟수만큼 실행되는 CFRunLoop 기반 타이머 생성
     @discardableResult
-    public class func schedule(repeatInterval: TimeInterval, delayStart: Bool = true, _ handler: @escaping (Timer?) -> Void) -> Timer {
+    public static func schedule(repeatInterval: TimeInterval, repeatCount: Int = 0, delayStart: Bool = true, _ handler: @escaping (Timer, Int) -> Void) -> Timer {
+        var count = 0
         var fireDate: CFAbsoluteTime = CFAbsoluteTimeGetCurrent()
+
         if delayStart {
             fireDate += repeatInterval
         }
-        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, repeatInterval, 0, 0, handler)
+
+        var timer: Timer!
+        timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, repeatInterval, 0, 0) { runLoopTimer in
+            count += 1
+            handler(timer!, count)
+
+            if repeatCount != 0, count >= repeatCount {
+                CFRunLoopTimerInvalidate(runLoopTimer) // 타이머 중지
+            }
+        }
+
         CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.commonModes)
-        return timer!
+        return timer
     }
 
     ///   Run function after x seconds
     @discardableResult
-    public class func schedule(delay: TimeInterval, _ handler: @escaping (Timer?) -> Void) -> Timer {
+    public static func schedule(delay: TimeInterval, _ handler: @escaping (Timer?) -> Void) -> Timer {
         let fireDate: CFAbsoluteTime = delay + CFAbsoluteTimeGetCurrent()
         let timer: CFRunLoopTimer? = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, handler)
         CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.commonModes)
         return timer!
     }
+
 
 }
